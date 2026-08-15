4e6b3bd5c19b5aafe372d81e05e8db7cd36c3627
AHA705 <78021303+AHA705@users.noreply.github.com>
2026-08-15 19:13:08 +0100

    Maze solution by following the wall

---
+++ maze.py
@@ -0,0 +1,48 @@
+# Maze solution. Goes through the maze and "follows" the wall until it finds the treasure
+import drone_hats
+
+axis = [North, East, South, West]
+
+
+def setup_maze():
+    plant(Entities.Bush)
+    substance = get_world_size() * 2 ** (num_unlocked(Unlocks.Mazes) - 1)
+    use_item(Items.Weird_Substance, substance)
+
+
+# Task for each drone
+def task(i, hat):
+    quick_print(i, "Drone reporting in, wearing:", hat)
+    change_hat(hat)
+    find_treasure(i)
+
+
+def find_treasure(i):
+    # Using i so that every drone goes into a different direction at start
+    i = i % 4
+    while True:
+        if get_entity_type() == Entities.Treasure:
+            harvest()
+            setup_maze()
+        if can_move(axis[i]):
+            move(axis[i])
+            # Turn left
+            i = (i - 1) % 4
+        else:
+            # Turn Right
+            i = (i + 1) % 4
+
+
+def main():
+    clear()
+    setup_maze()
+    u_hats: list = drone_hats.unlocked_hats()
+    max_d = max_drones() - 1
+    for i in range(max_d):
+        hat = u_hats[i % len(u_hats)]
+        spawn_drone(task, i, hat)
+    task(max_d, u_hats.pop())
+
+
+if __name__ == "__main__":
+    main()
